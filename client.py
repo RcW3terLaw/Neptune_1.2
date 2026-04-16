d715976a7d2d806bf83f7ab326ec912dfaab32c9
S8ap <136022053+RcW3terLaw@users.noreply.github.com>
2026-04-16 16:35:43 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,49 @@
+import socks
+import subprocess
+import time
+
+ONION = "oabtt5q4uqwsienlw55r7rcdveeudjdixpm4fsq6zlzz262gvtxqnfyd.onion"
+PORT = 1234
+
+tor = subprocess.Popen(
+    [".\\payload\\TorClient\\tor.exe", "-f", ".\\payload\\TorClient\\torrc"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    text=True
+)
+
+booted = False
+
+for line in tor.stdout:
+    print("[TOR]", line.strip())
+
+    if "Bootstrapped 100%" in line:
+        booted = True
+        break
+
+if booted:
+    time.sleep(1)
+
+    s = socks.socksocket()
+    s.set_proxy(socks.SOCKS5, "127.0.0.1", 9050) 
+    s.settimeout(10)
+
+    print("[CLIENT]Connexion à :", ONION)
+
+    s.connect((ONION, PORT))
+    print("[CLIENT]Connecté")
+
+    while True:
+        try:
+            data = s.recv(1024)
+            if not data:
+                break
+
+            msg = data.decode()
+            print(msg)
+
+            s.send(f"[CLIENT] received: {msg}".encode())
+
+        except Exception as e:
+            print("Erreur:", e)
+            break
