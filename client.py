9da16f6698459abdc591fb5c28e17be7474cede2
S8ap <136022053+RcW3terLaw@users.noreply.github.com>
2026-04-16 12:59:13 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -4,14 +4,26 @@ ONION = "oabtt5q4uqwsienlw55r7rcdveeudjdixpm4fsq6zlzz262gvtxqnfyd.onion"
 PORT = 1234
 
 s = socks.socksocket()
-s.set_proxy(socks.SOCKS5, "127.0.0.1", 9052) 
+s.set_proxy(socks.SOCKS5, "127.0.0.1", 9052)
+s.settimeout(10)
 
 print("Connexion à", ONION)
 
 s.connect((ONION, PORT))
 print("Connecté !")
 
-while True:    
-    data = s.recv(1024)
-    print(data.decode())
-    s.send(f"[CLIENT] Correctly recieve {data.decode()}".encode())
+while True:
+    try:
+        data = s.recv(1024)
+
+        if not data:
+            break
+
+        msg = data.decode()
+        print(msg)
+
+        s.send(f"[CLIENT] received: {msg}".encode())
+
+    except Exception as e:
+        print("Erreur:", e)
+        break
